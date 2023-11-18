784246a071976a8557feeff0ce39f95cc6f27b83
derrickmomanyi <rickydee38@gmail.com>
2023-11-18 12:53:24 +0300

    Added service generator

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -43,4 +45,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        $logOptions = LogOptions::defaults();
+        $logOptions->logName = 'Users'; // Set the custom log name
+        // $logOptions->ignoreChangedAttributes = ['last_login_at', 'updated_at'];
+        $logOptions->logOnlyDirty = true;
+        $logOptions->logAttributes = ['*'];
+        $logOptions->logAttributesToIgnore = ['password'];
+        return $logOptions;
+    }
 }
