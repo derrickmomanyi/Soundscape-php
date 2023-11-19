e583e4bad4e61d00c6cca638fc51971c77a5cc64
derrickmomanyi <rickydee38@gmail.com>
2023-11-19 10:47:04 +0300

    Added seeder to deploymen file

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,3 +11,20 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo 'Starting database seeding'
+php artisan db:seed
+echo 'Completed database seeding'
+
+
+# Testing
+echo 'Starting tests'
+composer test
+echo 'Completed tests'
+
+# Workers
+echo 'Restarting queues'
+php artisan queue:restart
+echo 'Queue restarted'
+
+echo 'Deployment completed. If any errors were encountered, consider rolling back the update immediately'
